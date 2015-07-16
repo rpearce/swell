44648b7d8095c0351f085fce162b8e46cbdc99a3
Robert Pearce <robertwaltonpearce@gmail.com>
2015-07-16 11:17:47 -0400

    create/destroy

--- swell-backend/app/controllers/api/surfers_controller.rb
+++ swell-backend/app/controllers/api/surfers_controller.rb
@@ -3,4 +3,25 @@ class Api::SurfersController < ApplicationController
     @surfers = Surfer.all
     render json: @surfers
   end
+
+  def create
+    @surfer = Surfer.new(surfer_params)
+    if @surfer.save
+      render status: 201
+    else
+      render json: @surfer.errors, status: 422
+    end
+  end
+
+  def destroy
+    @surfer = Surfer.find(params[:id])
+    @surfer.destroy
+    render head: :no_content
+  end
+
+  private
+
+  def surfer_params
+    params.require(:surfer).permit(:name, :age, :picture, :hometown)
+  end
 end
